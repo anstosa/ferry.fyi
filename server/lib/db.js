6373563a1e4c86b748e9abc423b4320b6dcbecb3
Ansel Santosa <anstosa@gmail.com>
2019-06-27 09:20:53 -0700

    Fix database connection in production

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,8 +1,25 @@
 import Sequelize from 'sequelize';
 
+const DIALECT_SQLITE = 'sqlite';
+const DIALECT_POSTGRES = 'postgres';
+
 export const {Op} = Sequelize;
 
-console.log(process.env.DATABASE_URL);
-export const db = new Sequelize(process.env.DATABASE_URL, {logging: false});
+const [, dialect] = process.env.DATABASE_URL.match(/^(\w+):\/{1,2}(.*)$/);
+
+let db = null;
+if (dialect === DIALECT_SQLITE) {
+    db = new Sequelize(process.env.DATABASE_URL, {logging: false});
+} else if (dialect === DIALECT_POSTGRES) {
+    db = new Sequelize(process.env.DATABASE_URL, {
+        dialect: DIALECT_POSTGRES,
+        dialectOptions: {
+            ssl: true,
+        },
+        logging: false,
+        protocol: DIALECT_POSTGRES,
+    });
+}
+export {db};
 
 export const dbInit = db.authenticate();
