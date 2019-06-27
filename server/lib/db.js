f316e64c506c224097737db4318957af1c246342
Ansel Santosa <anstosa@gmail.com>
2019-06-27 08:49:01 -0700

    Test config vars

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 
 export const {Op} = Sequelize;
 
+console.log(process.env.DATABASE_URL);
 export const db = new Sequelize(process.env.DATABASE_URL, {logging: false});
 
 export const dbInit = db.authenticate();
