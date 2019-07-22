beefa7c2acd027bdbeaa9b3aa3bd57141c30145f
Ansel Santosa <anstosa@gmail.com>
2019-07-22 07:24:54 -0700

    Cache webfonts

--- client/service-worker.js
+++ client/service-worker.js
@@ -5,7 +5,14 @@ workbox.core.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 workbox.googleAnalytics.initialize();
 
+// cache all first-party requests
 workbox.routing.registerRoute(
     new RegExp('/.*'),
     new workbox.strategies.NetworkFirst()
+);
+
+// cache fonts
+workbox.routing.registerRoute(
+    new RegExp('https://fonts.googleapis.com/.*'),
+    new workbox.strategies.CacheFirst()
 );
