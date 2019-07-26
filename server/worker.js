e15d2d711c0b9102eca6af3e3a7de17f7d6090d1
Ansel Santosa <anstosa@gmail.com>
2019-07-26 16:23:57 -0700

    Keep redis tidy

--- server/worker.js
+++ server/worker.js
@@ -2,6 +2,13 @@ import {updateLong, updateShort} from './lib/wsf-worker';
 import logger from 'heroku-logger';
 import Queue from 'bull';
 
+const removeJob = (job) => job.remove();
+
+async function clean(queue) {
+    await queue.clean(0, 'completed');
+    await queue.clean(0, 'failed');
+}
+
 (async () => {
     logger.info('Warming cache');
     await updateLong();
@@ -9,9 +16,15 @@ import Queue from 'bull';
 
     logger.info('Initilizing longUpdate processor');
     const longUpdate = new Queue('long', process.env.REDIS_URL);
+    await clean(longUpdate);
     longUpdate.process(() => updateLong());
+    longUpdate.on('completed', removeJob);
+    longUpdate.on('failed', removeJob);
 
     logger.info('Initilizing shortUpdate processor');
     const shortUpdate = new Queue('short', process.env.REDIS_URL);
+    await clean(shortUpdate);
     shortUpdate.process(() => updateShort());
+    shortUpdate.on('completed', removeJob);
+    shortUpdate.on('failed', removeJob);
 })();
