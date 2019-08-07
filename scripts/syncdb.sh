1d2b115f5c977f4cfb5a4ce13f0896fe61fade56
Ansel Santosa <anstosa@gmail.com>
2019-08-07 08:23:25 -0700

    Add script to sync local DB

---
+++ scripts/syncdb.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm latest.dump
+heroku pg:backups:capture --app=ferry-fyi
+heroku pg:backups:download --app=ferry-fyi
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d ferryfyi latest.dump
