74f1a511504ccdc50cd8f6c578bca250dae6d151
Ansel Santosa <anstosa@gmail.com>
2019-08-09 06:31:16 -0700

    Fail syncdb script more gracefully

--- scripts/syncdb.sh
+++ scripts/syncdb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-rm latest.dump
+set -e
+
+rm -f latest.dump
 heroku pg:backups:capture --app=ferry-fyi
 heroku pg:backups:download --app=ferry-fyi
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d ferryfyi latest.dump
