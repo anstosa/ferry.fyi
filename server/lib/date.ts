e3b36edf54f1620049ebbc9c3d44cff7e3b1e7e5
Ansel Santosa <anstosa@users.noreply.github.com>
2021-01-16 13:55:03 -0800

    debug

--- server/lib/date.ts
+++ server/lib/date.ts
@@ -15,6 +15,7 @@ export const wsfDateToTimestamp = (wsfDate?: string): number => {
 // "today" is a tricky concept. WSF's "day" ends around 3am
 export const getToday = (): string => {
   let date = DateTime.local().setZone("America/Los_Angeles");
+  console.log(date.toISO());
   if (date.hour < 3) {
     date = date.minus({ days: 1 });
   }
