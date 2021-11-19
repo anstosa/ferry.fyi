0f51cb113308512c5208f4621be6ed9d52df2c53
Ansel Santosa <anstosa@users.noreply.github.com>
2021-11-19 10:05:37 -0800

    Update icon and Android build

--- app/build/generated/source/buildConfig/release/fyi/ferry/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/fyi/ferry/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "fyi.ferry";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 2;
-  public static final String VERSION_NAME = "2";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "2.5";
 }
