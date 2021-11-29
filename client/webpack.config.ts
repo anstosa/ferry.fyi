220894f5f5d5ed45d4b77bb7d83e96b2decb63a7
Ansel Santosa <anstosa@users.noreply.github.com>
2021-11-29 15:11:49 -0800

    Allow local network access

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -57,6 +57,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     hot: true,
+    host: "0.0.0.0",
     port: 3000,
     devMiddleware: {
       publicPath: process.env.ROOT_URL,
