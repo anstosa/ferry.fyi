c0b3ab7226303e7c6b73fd2dfb258b5d95cbdae3
Ansel Santosa <568242+anstosa@users.noreply.github.com>
2022-03-28 06:14:47 -0700

    Add tomorrow schedule

--- client/views/Today.tsx
+++ client/views/Today.tsx
@@ -13,6 +13,7 @@ import type { Schedule } from "shared/contracts/schedules";
 
 export const Today = (): ReactElement => {
   const [schedule, setSchedule] = useState<Schedule | null>(null);
+  const [nextSchedule, setNextSchedule] = useState<Schedule | null>(null);
   const [isUpdating, setUpdating] = useState<boolean>(false);
   const [[terminal, mate], setTerminals] = useState<Array<Terminal | null>>([
     null,
@@ -37,14 +38,22 @@ export const Today = (): ReactElement => {
     setRoute("clinton", "mukilteo");
   }, []);
 
+  const now = DateTime.local();
+
   const updateSchedule = async (): Promise<void> => {
     if (isUpdating || !terminal || !mate) {
       return;
     }
     setUpdating(true);
-    const { schedule } = await getSchedule(terminal, mate, DateTime.local());
+    const { schedule } = await getSchedule(terminal, mate, now);
     setSchedule(schedule);
     setUpdating(false);
+    const { schedule: nextSchedule } = await getSchedule(
+      terminal,
+      mate,
+      now.plus({ days: 1 })
+    );
+    setNextSchedule(nextSchedule);
   };
 
   useEffect(() => {
@@ -55,23 +64,33 @@ export const Today = (): ReactElement => {
     return <Splash />;
   }
 
-  let firstBoat: null | string = null;
-  let boatCount = 0;
-  for (const slot of schedule.slots) {
-    if (slot.hasPassed) {
-      continue;
-    }
-    const boat = slot.vessel.id;
-    if (isNull(firstBoat)) {
-      firstBoat = boat;
+  const getBoatCount = (schedule: Schedule): number => {
+    let firstBoat: null | string = null;
+    let boatCount = 0;
+    for (const slot of schedule.slots) {
+      if (slot.hasPassed) {
+        continue;
+      }
+      const boat = slot.vessel.id;
+      if (isNull(firstBoat)) {
+        firstBoat = boat;
+        boatCount++;
+        continue;
+      }
+      if (boat === firstBoat) {
+        break;
+      }
       boatCount++;
-      continue;
     }
-    if (boat === firstBoat) {
-      break;
-    }
-    boatCount++;
+    return boatCount;
+  };
+
+  const todayCount = getBoatCount(schedule);
+  let nextCount: number | null = null;
+  if (nextSchedule && now.hour >= 22) {
+    nextCount = getBoatCount(nextSchedule);
   }
+  console.log(todayCount, nextCount);
 
   return (
     <>
@@ -84,7 +103,7 @@ export const Today = (): ReactElement => {
           "fixed inset-0 h-full p-8",
           "text-white text-center",
           "flex flex-col items-center justify-between",
-          { "bg-green-dark": boatCount === 2, "bg-red-dark": boatCount === 1 }
+          { "bg-green-dark": todayCount === 2, "bg-red-dark": todayCount === 1 }
         )}
       >
         <div />
@@ -98,14 +117,28 @@ export const Today = (): ReactElement => {
             project)
           </span>
         </div>
-        <h2 className="text-9xl font-bold flex items-start relative">
-          {boatCount}{" "}
-          <span
-            className={clsx("text-xl", "-mr-6 mt-4", "absolute top-0 right-0")}
-          >
-            *
-          </span>
-        </h2>
+        <div className="flex flex-col items-center">
+          <h2 className="text-9xl font-bold flex items-start relative">
+            {todayCount}{" "}
+            <span
+              className={clsx(
+                "text-xl",
+                "-mr-6 mt-4",
+                "absolute top-0 right-0"
+              )}
+            >
+              *
+            </span>
+          </h2>
+          {!isNull(nextCount) && (
+            <span className={clsx("text-xl mt-4")}>
+              {todayCount === 2 && nextCount === 1 && "But 1 tomorrow..."}
+              {todayCount === 2 && nextCount === 2 && "And 2 tomorrow!"}
+              {todayCount === 1 && nextCount === 1 && "And 1 tomorrow..."}
+              {todayCount === 1 && nextCount === 2 && "But 2 tomorrow!"}
+            </span>
+          )}
+        </div>
         <div className="flex flex-col gap-2">
           * but that could change!{" "}
           <Link to="/clinton" className="link">
