5bff627ca5f13de557a970207208a25e6a257a13
Ansel Santosa <568242+anstosa@users.noreply.github.com>
2023-01-12 06:19:51 -0800

    Try again

--- server/lib/firebase.ts
+++ server/lib/firebase.ts
@@ -1,3 +1,4 @@
+import { isKeyOf, isObject } from "shared/lib/objects";
 import admin from "firebase-admin";
 
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
@@ -14,3 +15,20 @@ export const firebase = admin.initializeApp({
     )
   ),
 });
+
+export const hasFirebaseCode = (error: unknown, code: string): boolean => {
+  if (!isObject(error)) {
+    return false;
+  }
+  if (isKeyOf(error, "code") && error.code === code) {
+    return true;
+  }
+  if (
+    isKeyOf(error, "errorInfo") &&
+    isKeyOf(error.errorInfo, "code") &&
+    error.errorInfo.code === code
+  ) {
+    return true;
+  }
+  return false;
+};
--- server/lib/push.ts
+++ server/lib/push.ts
@@ -1,8 +1,7 @@
 import { auth0 } from "./auth0";
 import { delay } from "./time";
-import { firebase } from "./firebase";
-import { FirebaseError } from "@firebase/util";
-import { Message } from "firebase-admin/messaging";
+import { firebase, hasFirebaseCode } from "./firebase";
+import { Message } from "firebase-admin/lib/messaging/messaging-api";
 
 const MAX_RERTY_TIME = 10 * 1000;
 
@@ -19,19 +18,19 @@ const trySend = async (): Promise<void> => {
       await firebase.messaging().send(message);
       retryTime = 1;
     } catch (error: unknown) {
-      if (error instanceof FirebaseError) {
-        if (error.code === "messaging/registration-token-not-registered") {
-          console.warn(
-            `Deleting expired push token for user ${message.data?.userId}`
+      if (
+        hasFirebaseCode(error, "messaging/registration-token-not-registered")
+      ) {
+        console.warn(
+          `Deleting expired push token for user ${message.data?.userId}`
+        );
+        if (message.data?.userId) {
+          auth0.updateAppMetadata(
+            { id: message.data?.userId },
+            {
+              fcmToken: null,
+            }
           );
-          if (message.data?.userId) {
-            auth0.updateAppMetadata(
-              { id: message.data?.userId },
-              {
-                fcmToken: null,
-              }
-            );
-          }
         }
       } else if (retryTime <= MAX_RERTY_TIME) {
         retryTime *= 2;
